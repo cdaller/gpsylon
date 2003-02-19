3c05dd99a9fa7be0bddd3afc0d5407fdeae641a7
Christof Dallermassl <christof@dallermassl.at>
2003-02-19 14:40:49 +0000

    initial version

---
+++ plugins/mapmanager/src/org/dinopolis/gpstool/plugin/mapmanager/MapInfoHoldingTable.java
@@ -0,0 +1,42 @@
+/***********************************************************************
+ * @(#)$RCSfile$   $Revision$$Date$
+ *
+ * Copyright (c) 2003 IICM, Graz University of Technology
+ * Inffeldgasse 16c, A-8010 Graz, Austria.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ ***********************************************************************/
+
+
+
+package org.dinopolis.gpstool.plugin.mapmanager;
+import org.dinopolis.gpstool.MapInfo;
+
+/**
+ * Helper interface for all table like classes that hold MapInfo objects in
+ * rows.
+ * 
+ * @author cdaller
+ */
+public interface MapInfoHoldingTable
+{
+	
+	/**
+	 * Return the MapInfo object of in the given row.
+	 * 
+	 */
+	MapInfo getMapInfo(int row);
+}
---
+++ plugins/mapmanager/src/org/dinopolis/gpstool/plugin/mapmanager/MapInfoTableSorter.java
@@ -0,0 +1,64 @@
+
+/***********************************************************************
+ * @(#)$RCSfile$   $Revision$$Date$
+ *
+ * Copyright (c) 2003 IICM, Graz University of Technology
+ * Inffeldgasse 16c, A-8010 Graz, Austria.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ ***********************************************************************/
+
+
+package org.dinopolis.gpstool.plugin.mapmanager;
+
+import org.dinopolis.gpstool.MapInfo;
+import org.dinopolis.gpstool.gui.util.TableSorter;
+
+/**
+ * @author cdaller
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class MapInfoTableSorter extends TableSorter implements MapInfoHoldingTable
+{
+	
+	/**
+	 * Constructor for MapInfoTableSorter.
+	 */
+	public MapInfoTableSorter()
+	{
+		super();
+	}
+
+	/**
+	 * Constructor for MapInfoTableSorter.
+	 * @param model
+	 */
+	public MapInfoTableSorter(MapInfoTableModel model)
+	{
+		super(model);
+	}
+
+	public MapInfo getMapInfo(int row)
+	{
+		checkModel();
+		return(((MapInfoTableModel)model).getMapInfo(sorted_indices_[row]));
+	}
+
+}

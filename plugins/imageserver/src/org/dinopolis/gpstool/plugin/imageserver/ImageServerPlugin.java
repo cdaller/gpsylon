dab1e07777418281d2550c6a575b4ca5d8193169
Christof Dallermassl <christof@dallermassl.at>
2003-07-01 08:16:11 +0000

    Initial revision

---
+++ plugins/imageserver/src/org/dinopolis/gpstool/plugin/imageserver/ImageServerPlugin.java
@@ -0,0 +1,392 @@
+/***********************************************************************
+ * @(#)$RCSfile$   $Revision$$Date$
+ *
+ * Copyright (c) 2003 IICM, Graz University of Technology
+ * Inffeldgasse 16c, A-8010 Graz, Austria.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., a
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ ***********************************************************************/
+
+package org.dinopolis.gpstool.plugin.imageserver;
+
+import com.bbn.openmap.Layer;
+import java.awt.event.ActionEvent;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import org.dinopolis.gpstool.GPSMap;
+import org.dinopolis.gpstool.TrackManager;
+import org.dinopolis.gpstool.gui.MouseMode;
+import org.dinopolis.gpstool.plugin.GuiPlugin;
+import org.dinopolis.gpstool.plugin.PluginSupport;
+import org.dinopolis.gpstool.track.Track;
+import org.dinopolis.gpstool.track.TrackImpl;
+import org.dinopolis.gpstool.track.Trackpoint;
+import org.dinopolis.gpstool.track.TrackpointImpl;
+import org.dinopolis.util.Debug;
+import org.dinopolis.util.ResourceManager;
+import org.dinopolis.util.Resources;
+import org.dinopolis.util.gui.ActionStore;
+import org.dinopolis.util.gui.MenuFactory;
+
+
+//----------------------------------------------------------------------
+/**
+ * This plugin uses a very simple webserver to listent to HTTP
+ * requests and answers them with a simple default page and a
+ * screenshot of the GPSylon window.
+ *
+ * @author Christof Dallermassl
+ * @version $Revision$
+ */
+
+public class ImageServerPlugin implements GuiPlugin
+{
+	protected JMenuItem plugin_menu_item_;
+	/** the resources of the track plugin */
+	protected Resources resources_;
+	/** the resources of the GPSMap application */
+	protected Resources application_resources_;
+	/** the action store */
+	protected ActionStore action_store_;
+
+  protected PluginSupport plugin_support_;
+
+	// keys for resources:
+	public static final String KEY_IMAGESERVER_PLUGIN_IDENTIFIER =
+		"imageserver.plugin.identifier";
+	public static final String KEY_IMAGESERVER_PLUGIN_VERSION =
+		"imageserver.plugin.version";
+	public static final String KEY_IMAGESERVER_PLUGIN_NAME =
+		"imageserver.plugin.name";
+	public static final String KEY_IMAGESERVER_PLUGIN_DESCRIPTION =
+		"imageserver.plugin.description";
+
+//	public static final String KEY_IMAGESERVER_SHOW_MENU_NAME = "imageserver_show";
+
+	/** the name of the resource file */
+	private final static String RESOURCE_BUNDLE_NAME = "ImageServerPlugin";
+
+	/** the name of the directory containing the resources */
+	private final static String USER_RESOURCE_DIR_NAME = GPSMap.USER_RESOURCE_DIR_NAME;
+
+	public static final String IMAGESERVER_ACTION_STORE_ID = RESOURCE_BUNDLE_NAME;
+
+      // imageserver actions 
+//  public final static String ACTION_SHOW_IMAGESERVER = "imageserver_show";
+
+  public static final String KEY_IMAGESERVER_WEB_SERVER_PORT = "imageserver.web_server.port";
+  public static final String KEY_IMAGESERVER_WEB_SERVER_DEFAULT_PAGE = "imageserver.web_server.default_page";
+  public static final String KEY_IMAGESERVER_WEB_SERVER_SCREENSHOT_URL = "imageserver.web_server.screenshot_url";
+
+  protected WebServer web_server_;
+
+  
+	// ----------------------------------------------------------------------  
+	// Implementation of org.dinopolis.gpstool.plugin.Plugin
+	// ----------------------------------------------------------------------  
+
+	//----------------------------------------------------------------------
+	/**
+	 * Initialize the plugin and pass a PluginSupport that provides
+	 * objects, the plugin may use.
+	 *
+	 * @param support the PluginSupport object
+	 */
+	public void initializePlugin(PluginSupport support)
+	{
+    plugin_support_ = support;
+		application_resources_ = support.getResources();
+
+		// load plugin resources:
+		if (Debug.DEBUG)
+			Debug.println("imageserverplugin_init", "loading resources");
+		loadResources();
+        // attach the plugin resources to the global application
+        // resources:
+    application_resources_.attachResources(resources_);
+ 
+		// prepare the actionstore for the menu:
+// 		action_store_ = ActionStore.getStore(IMAGESERVER_ACTION_STORE_ID);
+// 		action_store_.addActions(new Action[] { new ShowImageserverAction()});
+    
+//     loadTestTrack();
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * The application calls this method to indicate that the plugin is
+	 * activated and will be used from now on. The Plugin should
+	 * initialize any needed resources (files, etc.) in this method. This
+	 * implementation starts the web server thread.
+	 *
+	 * @throws Exception if an error occurs. If this method throws an
+	 * exception, the plugin will not be used by the application.
+	 */
+
+	public void startPlugin() throws Exception
+	{
+    web_server_ = new WebServer(resources_.getInt(KEY_IMAGESERVER_WEB_SERVER_PORT),
+				resources_.getString(KEY_IMAGESERVER_WEB_SERVER_DEFAULT_PAGE),
+				resources_.getString(KEY_IMAGESERVER_WEB_SERVER_SCREENSHOT_URL));
+    web_server_.setComponent(plugin_support_.getMapComponent());
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * The application calls this method to indicate that the plugin is
+	 * deactivated and will not be used any more. The Plugin should
+	 * release all resources (close files, etc.) in this method. This
+	 * implementation stops the webserver thread.
+	 *
+	 * @throws Exception if an error occurs.
+	 */
+
+	public void stopPlugin() throws Exception
+	{
+    web_server_.stopServer();
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * Returns the unique id of the plugin. The id is used to identify
+	 * the plugin and to distinguish it from other plugins.
+	 *
+	 * @return The id of the plugin.
+	 */
+
+	public String getPluginIdentifier()
+	{
+		return(resources_.getString(KEY_IMAGESERVER_PLUGIN_IDENTIFIER));
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * Returns the version of the plugin. The version may be used to
+	 * choose between different version of the same plugin. 
+	 *
+	 * @return The version of the plugin.
+	 */
+
+	public float getPluginVersion()
+	{
+		return ((float) resources_.getDouble(KEY_IMAGESERVER_PLUGIN_VERSION));
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * Returns the name of the Plugin. The name should be a human
+	 * readable and understandable name like "Save Image as JPEG". It is
+	 * prefereable but not necessary that the name is localized. 
+	 *
+	 * @return The name of the plugin.
+	 */
+
+	public String getPluginName()
+	{
+		return (resources_.getString(KEY_IMAGESERVER_PLUGIN_NAME));
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * Returns a description of the Plugin. The description should be
+	 * human readable and understandable like "This plugin saves the
+	 * content of the main window as an image in jpeg format". It is
+	 * prefereable but not necessary that the description is localized. 
+	 *
+	 * @return The description of the plugin.
+	 */
+
+	public String getPluginDescription()
+	{
+		return (resources_.getString(KEY_IMAGESERVER_PLUGIN_DESCRIPTION));
+	}
+
+	//----------------------------------------------------------------------
+	// GuiPlugin methods
+	//----------------------------------------------------------------------
+
+	//----------------------------------------------------------------------
+	/**
+	 * The plugin may return a JMenu object to be used in the main menu of
+	 * the application and may (should) contain other menu items. The
+	 * menuitems returned should provide an icon, a mnemonic key, and a
+	 * localized name (and a accelerator key).
+	 *
+	 * @return A menu that is used in the main menu in the
+	 * application or <code>null</code>, if no main menu is needed.
+	 *
+	 */
+	public JMenu getMainMenu()
+	{
+		return (null);
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * The application provides a sub menu for every plugin that may be
+	 * used. The JMenuItem (or JMenu) returned is added to a submenu in
+	 * the "plugins" menu item.  The menuitems returned should provide an
+	 * icon, a mnemonic key, and a localized name (and a accelerator key).
+	 *
+	 * @return A menuitem (or a JMenu) that are used in a sub menu in the
+	 * application or <code>null</code>, if no submenus are needed.
+	 *
+	 */
+	public JMenuItem getSubMenu()
+	{
+    return(null);
+// 		if (plugin_menu_item_ == null)
+// 		{
+// 			plugin_menu_item_ =
+// 				(JMenuItem) MenuFactory.createMenuComponent(
+// 					MenuFactory.KEY_MENUE_PREFIX,
+// 					KEY_IMAGESERVER_SHOW_MENU_NAME,
+// 					resources_,
+// 					action_store_);
+// 		}
+// 		return (plugin_menu_item_);
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * Every plugin may provide one or more mouse modes. These mouse modes
+	 * may react on mouse clicks, drags, etc.
+	 *
+	 * @return mouse modes that are used by this plugin in the application or
+	 * <code>null</code>, if no mouse modes are used.
+	 *
+	 */
+	public MouseMode[] getMouseModes()
+	{
+    return(null);
+// 		if (mouse_mode_ == null)
+// 		{
+// 			mouse_mode_ = new MapManagerMouseMode();
+// 			mouse_mode_.initialize(resources_, (MapManagerLayer)getLayer(),this,map_manager_);
+// 		}
+// 		return (new MouseMode[] { mouse_mode_ });
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * If the plugin wants to draw anything on the map it may
+	 * return a layer here or <code>null</code> if not.
+	 *
+	 * @return the layer the plugin wants to paint into.
+	 * @see com.bbn.openmap.Layer
+	 */
+
+	public Layer getLayer()
+	{
+		return (null);
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * Called by the application to switch the layer on or off. If the
+	 * layer is switched off, it must not paint anything and should not
+	 * consume any calculational power.
+	 *
+	 * @param active if <code>true</code> the layer is switched on and
+	 * should react on changes of the projection and draw anything in the
+	 * paintComponent method.
+	 */
+	public void setActive(boolean active)
+	{
+	}
+
+	//----------------------------------------------------------------------
+	/**
+	 * Returns if the plugin is active or not.
+	 *
+	 * @return <code>true</code> if the plugin is active and paints
+	 * something.
+	 */
+	public boolean isActive()
+	{
+		return (true);
+	}
+
+	//----------------------------------------------------------------------
+	// other methods
+	//----------------------------------------------------------------------
+
+	//----------------------------------------------------------------------
+	/**
+	 * Loads the resource file, or exits on a MissingResourceException.
+	 */
+
+	void loadResources()
+	{
+		try
+		{
+			resources_ =
+				ResourceManager.getResources(
+					ImageServerPlugin.class,
+					RESOURCE_BUNDLE_NAME,
+					USER_RESOURCE_DIR_NAME,
+					Locale.getDefault());
+		}
+		catch (MissingResourceException mre)
+		{
+			if (Debug.DEBUG)
+				Debug.println(
+					"ImageserverPlugin",
+					mre.toString() + '\n' + Debug.getStackTrace(mre));
+			System.err.println(
+				"ImageserverPlugin: resource file '"
+					+ RESOURCE_BUNDLE_NAME
+					+ "' not found");
+			System.err.println(
+				"please make sure that this file is within the classpath !");
+			System.exit(1);
+		}
+	}
+
+
+// //----------------------------------------------------------------------
+// /**
+//  * The Action that triggers the track chart window.
+//  */
+
+//   class ShowImageserverAction extends AbstractAction 
+//   {
+//         //----------------------------------------------------------------------
+//         /**
+//          * The Default Constructor.
+//          */
+
+//     public ShowImageserverAction()
+//     {
+//       super(ACTION_SHOW_IMAGESERVER);
+//     }
+
+//         //----------------------------------------------------------------------
+//         /**
+//          * Show the track chart
+//          * 
+//          * @param event the action event
+//          */
+
+//     public void actionPerformed(ActionEvent event)
+//     {
+//     }
+//   }
+}
---
+++ plugins/imageserver/src/org/dinopolis/gpstool/plugin/imageserver/WebServer.java
@@ -0,0 +1,292 @@
+/***********************************************************************
+ * @(#)$RCSfile$   $Revision$$Date$
+ *
+ * Copyright (c) 2003 IICM, Graz University of Technology
+ * Inffeldgasse 16c, A-8010 Graz, Austria.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., a
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ ***********************************************************************/
+
+package org.dinopolis.gpstool.plugin.imageserver;
+
+import java.awt.Component;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.imageio.ImageIO;
+
+
+//----------------------------------------------------------------------
+/**
+ * This class is a very simple (but multithreaded) webserver. The only content
+ * it provides is a default page and a screenshot of a Component.
+ *
+ * @author Christof Dallermassl
+ * @version $Revision$
+ */
+
+public class WebServer extends Thread
+{
+  protected int port_;
+  public static final int DEFAULT_PORT = 10080;
+  public static final int SERVER_SOCKET_TIMEOUT = 60000;  // in milliseconds (1min)
+  protected ServerSocket server_socket_;
+  protected boolean thread_run_ = true;
+  protected Component component_;
+  protected String default_page_;
+  protected SimpleDateFormat date_format_ = new SimpleDateFormat("EEE, dd-MMM-yy HH:mm:ss z");
+  protected String screenshot_url_;
+
+//----------------------------------------------------------------------
+/**
+ * Initializes and starts the listener threads.
+ *
+ * @param port the port to listen to.
+ * @param default_page the default html page to return on request to
+ * "/" or "index.html".
+ * @param screenshot_filename the filename that provides the
+ * screenshot.
+ */
+  public WebServer(int port, String default_page, String screenshot_filename)
+  {
+    default_page_ = default_page;
+    screenshot_url_ = screenshot_filename;
+    try
+    {
+      System.err.println("starting webserver on port "+port);
+      server_socket_ = new ServerSocket (port);
+      start();
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+
+//----------------------------------------------------------------------
+/**
+ * Stops the listener threads.
+ */
+  public void stopServer()
+  {
+    thread_run_ = false;
+  }
+
+//----------------------------------------------------------------------
+/**
+ * Set the component that is used to create the screeshot.
+ *
+ * @param component the component to produce a screenshot from.
+ */
+  public void setComponent(Component component)
+  {
+    component_ = component;
+  }
+
+
+
+  public void run()
+  {
+    try
+    {
+      server_socket_.setSoTimeout(SERVER_SOCKET_TIMEOUT);
+      while (thread_run_)
+      {
+        try
+        {
+          new WebServerClientConnection(server_socket_.accept() );
+        }
+        catch (InterruptedIOException iioe)
+        {
+        }
+      }
+    }
+    catch (IOException ioe)
+    {
+    }
+    try
+    {
+      server_socket_.close();
+    }
+    catch (IOException ioe)
+    {
+    }
+  }
+
+
+//----------------------------------------------------------------------
+/**
+ * Returns an image of the component.
+ *
+ * @return an image of the component.
+ */
+  public static BufferedImage getComponentScreenShot(Component component)
+  {
+    BufferedImage image = (BufferedImage)component.createImage(component.getWidth(),component.getHeight());
+    Graphics2D g2 = image.createGraphics();
+    component.paint(g2);
+    return (image);
+  }
+
+
+//----------------------------------------------------------------------
+/**
+ * Writes the data to the given output stream.
+ *
+ * @param component the component to write.
+ * @param out the outputstream to write the data to.
+ * @throws IOException if an error occurs during writing.
+ */
+  public void writeComponent(Component component, OutputStream out)
+    throws IOException
+  {
+    BufferedImage image = getComponentScreenShot(component);
+    ImageIO.write(image,"PNG",out);
+  }
+
+  
+
+  public static void main(String[] args)
+  {
+    WebServer server = new WebServer(DEFAULT_PORT,"no default page","gpsylon.png");
+  }
+  
+//----------------------------------------------------------------------
+// inner class
+/**
+ */
+  class WebServerClientConnection extends Thread
+  {
+    protected Socket socket_;
+    protected PrintStream output_;
+    protected BufferedReader input_;
+  
+    WebServerClientConnection (Socket socket)
+    {
+      socket_ = socket;
+      start();
+    }
+  
+  
+    public void run()
+    {
+      try
+      {
+        output_ = new PrintStream (socket_.getOutputStream());
+        InputStream input_stream = socket_.getInputStream ();
+        input_ = new BufferedReader(new InputStreamReader(input_stream));
+
+        String request = input_.readLine();
+        System.err.println("original requesting "+request);
+        String dummy = input_.readLine(); 
+        while ( (dummy!=null) && (!dummy.trim().equals("")) )
+        { 
+          dummy = input_.readLine();
+          System.err.println ("rest of request:"+dummy);
+        }
+
+
+        StringTokenizer st = new StringTokenizer( request );
+        if ( (st.countTokens() >= 2) && st.nextToken().equals("GET"))
+        {
+          if ( (request = st.nextToken()).startsWith("/") )
+            request = request.substring( 1 );
+
+          if (request.equals(""))
+            request = "/";
+          
+          System.err.println("requesting '"+request+"'");
+
+          if(request.equals("index.html") || request.equals("/"))
+          {
+            StringBuffer html_page = new StringBuffer();
+            html_page.append("<HTML><HEAD><TITLE>index.html</title></HEAD>");
+            html_page.append("<body>this is the body</body>");
+            html_page.append("</HTML>");
+            
+            output ("HTTP/1.0 200 Document Follows");
+            output ("Server: Simple HTTP Server");
+            output ("Date: "+date_format_.format(new Date()));
+            
+            String mime_type = "text/html";
+            output ("Content-Type: "+mime_type);
+            output ("Content-Length: " + default_page_.length());
+            output ("Last-Modified: Sunday, 18-May-97 23:59:59 GMT");
+            output ("");
+            output (default_page_);
+          }
+          else if (request.equals(screenshot_url_))
+          {
+            System.out.println("return screenshot of component "+component_);
+            if(component_ != null)
+            {
+              ByteArrayOutputStream out = new ByteArrayOutputStream();
+              writeComponent(component_,out);
+              
+              output ("HTTP/1.0 200 Document Follows");
+              output ("Server: Simple GPSylon HTTP Server");
+              output ("Date: "+date_format_.format(new Date()));
+              output ("Content-Type: image/png");
+              output ("Content-Length: "+out.size());
+              output ("");
+              output_.write(out.toByteArray());
+            }
+            else
+            {
+              output( "HTTP/1.0 404 Object not found.");
+              output("");
+            }
+            
+          }
+          else
+          {
+            output( "HTTP/1.0 404 Object not found.");
+            output("");
+          }
+        }
+
+        output_.flush ();
+        socket_.close();
+      }
+      catch ( Exception e )
+      {
+        e.printStackTrace();
+      }
+    }
+  
+    protected void output (String s)
+      throws IOException
+    {
+        output_.print (s + (char)13 + (char)10);
+        output_.flush();
+        System.out.println("Return: '" + s +"'\n");
+    }
+  }
+
+}

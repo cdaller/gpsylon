b1590b8d441964a74259c65dbfdf3dc7b8908e35
Christof Dallermassl <christof@dallermassl.at>
2003-07-01 08:52:17 +0000

    added status line and progress bar

--- src/org/dinopolis/util/gui/SplashScreen.java
+++ src/org/dinopolis/util/gui/SplashScreen.java
@@ -29,14 +29,18 @@ import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
 
 //----------------------------------------------------------------------
 /**
  * This class is used to visualize a simple splash screen icon for a
- * definable amount of time.
+ * definable amount of time. Additionally, a progress bar and a status
+ * line can be displayed.
  *
  * @author Dieter Freismuth
  * @version $Revision$
@@ -44,6 +48,10 @@ import javax.swing.SwingUtilities;
 
 public class SplashScreen extends JWindow 
 {
+
+  JProgressBar progress_bar_;
+  JLabel status_line_;
+  
   //----------------------------------------------------------------------
   /**
    * Constructor taking an Icon that is to be displayed and the time
@@ -57,6 +65,26 @@ public class SplashScreen extends JWindow
   public SplashScreen(Icon banner, int display_millis)
   {
     this(new JLabel(banner), display_millis);
+  }
+
+  //----------------------------------------------------------------------
+  /**
+   * Constructor taking an Icon that is to be displayed and the time
+   * to display this icon. Additionally, the min/max values for the
+   * progress bar can be set. If both are equal to zero, no progress
+   * bar and status line are displayed.
+   *
+   * @param banner the icon to display.
+   * @param display_millis the amount of milliseconds to display the
+   * banner.
+   * @param progress_min_value the min value for the progress bar.
+   * @param progress_max_value the max value for the progress bar.
+   */
+
+  public SplashScreen(Icon banner, int display_millis,
+                      int progress_min_value, int progress_max_value)
+  {
+    this(new JLabel(banner), display_millis, progress_min_value, progress_max_value);
   }
 
   //----------------------------------------------------------------------
@@ -76,6 +104,26 @@ public class SplashScreen extends JWindow
 
   //----------------------------------------------------------------------
   /**
+   * Constructor taking a String that is to be displayed and the time
+   * to display it. Additionally, the min/max values for the progress
+   * bar can be set. If both are equal to zero, no progress bar and
+   * status line are displayed.
+   *
+   * @param text the text to display.
+   * @param display_millis the amount of milliseconds to display the
+   * banner.
+   * @param progress_min_value the min value for the progress bar.
+   * @param progress_max_value the max value for the progress bar.
+   */
+
+  public SplashScreen(String text, int display_millis,
+                      int progress_min_value, int progress_max_value)
+  {
+    this(new JLabel(text), display_millis, progress_min_value, progress_max_value);
+  }
+
+  //----------------------------------------------------------------------
+  /**
    * Constructor taking a label that is to be displayed and the time
    * to display it.
    *
@@ -86,11 +134,40 @@ public class SplashScreen extends JWindow
 
   public SplashScreen(JLabel label, int display_millis)
   {
+    this(label,display_millis,0,0);
+  }
+
+  
+  //----------------------------------------------------------------------
+  /**
+   * Constructor taking a label that is to be displayed and the time
+   * to display it. Additionally, the min/max values for the progress
+   * bar can be set. If both are equal to zero, no progress bar and
+   * status line are displayed.
+   *
+   * @param label the label to display.
+   * @param display_millis the amount of milliseconds to display the
+   * banner.
+   * @param progress_min_value the min value for the progress bar.
+   * @param progress_max_value the max value for the progress bar.
+   */
+
+  public SplashScreen(JLabel label, int display_millis,
+                      int progress_min_value, int progress_max_value)
+  {
     getContentPane().add(label, BorderLayout.CENTER);
+    if((progress_min_value != 0) || (progress_max_value != 0))
+    {
+      JPanel status_panel = new JPanel(new BorderLayout());
+      status_panel.add(progress_bar_ = new JProgressBar(progress_min_value,progress_max_value),
+                       BorderLayout.CENTER);
+      status_panel.add(status_line_ = new JLabel(),BorderLayout.SOUTH);
+      getContentPane().add(status_panel,BorderLayout.SOUTH);
+    }
     pack();
     Dimension screen_size =
       Toolkit.getDefaultToolkit().getScreenSize();
-    Dimension label_size = label.getPreferredSize();
+    Dimension label_size = getContentPane().getPreferredSize();
     setLocation(screen_size.width/2 - (label_size.width/2),
                 screen_size.height/2 - (label_size.height/2));
     addMouseListener(new MouseAdapter() 
@@ -130,8 +207,46 @@ public class SplashScreen extends JWindow
           }
       };
       Thread splash = new Thread(waiter, "SplashThread");
+      splash.setDaemon(true);
       splash.start();
     }
+  }
+
+  //----------------------------------------------------------------------
+  /**
+   * Set the status and the current progress for the splash screen.
+   *
+   * @param text the text to be displayed in the status line.
+   * @param progress_value the percentage of the progress.
+   */
+  public void setStatus(String text, int progress_value)
+  {
+    setStatus(text);
+    setProgress(progress_value);
+  }
+
+  //----------------------------------------------------------------------
+  /**
+   * Set the status for the splash screen.
+   *
+   * @param text the text to be displayed in the status line.
+   */
+  public void setStatus(String text)
+  {
+    if(status_line_ != null)
+      status_line_.setText(text);
+  }
+
+  //----------------------------------------------------------------------
+  /**
+   * Set the current progress for the splash screen.
+   *
+   * @param progress_value the percentage of the progress.
+   */
+  public void setProgress(int progress_value)
+  {
+    if(progress_bar_ != null)
+      progress_bar_.setValue(progress_value);
   }
 
   //----------------------------------------------------------------------
@@ -141,7 +256,39 @@ public class SplashScreen extends JWindow
 
   public static void main(String[] arguments)
   {
-    new SplashScreen("test", -1);
+    SplashScreen splash = new SplashScreen(new ImageIcon("/filer/cdaller/texte/pic/cdaller.jpg"), 5000,0,100);
+    splash.setStatus("loading plugins...",10);
+    try
+    {
+      Thread.sleep(500);
+    }
+    catch(Exception ignore) {}
+    splash.setStatus("initialize plugins...",20);
+    try
+    {
+      Thread.sleep(500);
+    }
+    catch(Exception ignore) {}
+    splash.setStatus("start plugin 1...",30);
+    try
+    {
+      Thread.sleep(500);
+    }
+    catch(Exception ignore) {}
+    splash.setStatus("start another plugin ...",50);
+    try
+    {
+      Thread.sleep(500);
+    }
+    catch(Exception ignore) {}
+    splash.setStatus("finish first plugin...",70);
+    try
+    {
+      Thread.sleep(500);
+    }
+    catch(Exception ignore) {}
+    splash.setStatus("open window...",100);
+    
   }
 
 }

8444633b59892ab398cd75ecb3ca2c6368ff0242
Christof Dallermassl <christof@dallermassl.at>
2003-07-08 15:52:05 +0000

    initial checkin

---
+++ plugins/downloadmousemode/src/org/dinopolis/gpstool/plugin/downloadmousemode/Custom1Downloader.java
@@ -0,0 +1,47 @@
+/***********************************************************************
+ * @(#)$RCSfile$   $Revision$$Date$
+ *
+ * Copyright (c) 2003 IICM, Graz University of Technology
+ * Inffeldgasse 16c, A-8010 Graz, Austria.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ ***********************************************************************/
+
+
+package org.dinopolis.gpstool.plugin.downloadmousemode;
+
+
+//----------------------------------------------------------------------
+/**
+ *
+ * @author Christof Dallermassl
+ * @version $Revision$
+ */
+
+public class Custom1Downloader extends SimpleUrlDownloader
+{
+
+  public String getPluginName()
+  {
+    return("Custom1 Downloader");
+  }
+  
+  protected String getMapServerName()
+  {
+    return("custom1");
+  }
+  
+}

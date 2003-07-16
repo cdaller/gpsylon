38e984e4b7147cdfd5280736eba9336192b457d8
Christof Dallermassl <christof@dallermassl.at>
2003-07-16 14:28:22 +0000

    added javadoc

--- plugins/downloadmousemode/src/org/dinopolis/gpstool/plugin/downloadmousemode/SimpleUrlDownloader.java
+++ plugins/downloadmousemode/src/org/dinopolis/gpstool/plugin/downloadmousemode/SimpleUrlDownloader.java
@@ -154,7 +154,51 @@ public abstract class SimpleUrlDownloader implements MapRetrievalPlugin
     return("Downloads maps from "+getMapServerName()+" server.");
   }
 
+//----------------------------------------------------------------------
+/**
+ * Returns the scale the plugin would use for the given parameters.
+ *
+ * @param latitude the latitude of the center of the map to retrieve.
+ * @param longitude the longitude of the center of the map to retrieve.
+ * @param wanted_mapblast_scale the scale to download. The base for
+ * this value is the scale used by the mapblast server. The plugin has
+ * to calculate this value to match the scale used by its map source!
+ * @param image_width the width of the map to download.
+ * @param image_height the height of the map to download.
+ * @return the scale the plugin uses for the given parameters.
+ */
+  public double getMapScale(double latitude, double longitude,
+                            double wanted_mapblast_scale, int image_height,
+                            int image_width)
+  {
+    return(getDownloadScale(wanted_mapblast_scale));
+  }
 
+
+
+//----------------------------------------------------------------------
+/**
+ * Retrieve a map with the given parameters and store it in a file.
+ *
+ * @param latitude the latitude of the center of the map to retrieve.
+ * @param longitude the longitude of the center of the map to retrieve.
+ * @param wanted_mapblast_scale the scale to download. The base for
+ * this value is the scale used by the mapblast server. The plugin has
+ * to calculate this value to match the scale used by its map source!
+ * @param image_width the width of the map to download.
+ * @param image_height the height of the map to download.
+ * @param file_path_wo_extension the path of the file, the map should
+ * be store to. This path contains the directory and the filename, but
+ * not the extension of the file, as the extension may depend on the
+ * fileformat the plugin retrieves.
+ * @param progress_listener a progress listener to be informed about
+ * the progress on retrieval.
+ * @return a MapInfo object that holds the complete information about
+ * the downloaded map. This includes the complete filename (with
+ * extension) and the scale of the image in mapblast format!
+ * @throws IOException if an error on reading or writing of the map
+ * occured.
+ */
   public MapInfo getMap(double latitude, double longitude, double wanted_mapblast_scale,
                         int image_width, int image_height, String file_path_wo_extension,
                         ProgressListener progress_listener)

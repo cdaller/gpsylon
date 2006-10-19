d7a2c79643d045087ab681d886357ce38729f7f8
Christof Dallermassl <christof@dallermassl.at>
2006-10-19 08:07:52 +0000

    removed empty lines

--- auxiliary/org/dinopolis/gpstool/sql/create_location_derby.sql
+++ auxiliary/org/dinopolis/gpstool/sql/create_location_derby.sql
@@ -7,15 +7,12 @@
 # nothing to do
 
 # markers table:
-CREATE TABLE markers ( marker_id INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
-                       name VARCHAR (255), 
-                       latitude FLOAT NOT NULL, 
+CREATE TABLE markers ( marker_id INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+                       name VARCHAR (255),
+                       latitude FLOAT NOT NULL,
                        longitude FLOAT NOT NULL,
                        category_id VARCHAR(64),
                        level_of_detail INT DEFAULT 1);
 CREATE INDEX markers_name ON markers ( name );
 CREATE INDEX markers_cat ON markers ( category_id );
 CREATE INDEX markers_coordinates ON markers (latitude, longitude);
-
-
-
